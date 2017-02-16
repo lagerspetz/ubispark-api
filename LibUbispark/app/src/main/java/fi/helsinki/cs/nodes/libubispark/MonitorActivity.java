3461fd794c0757f508305121d3be35f05e6c7367
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2017-02-16 12:30:58 +0200

    Various.

--- LibUbispark/app/src/main/java/fi/helsinki/cs/nodes/libubispark/MonitorActivity.java
+++ LibUbispark/app/src/main/java/fi/helsinki/cs/nodes/libubispark/MonitorActivity.java
@@ -34,7 +34,7 @@ public class MonitorActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
